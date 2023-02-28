b1f4d89100cf443e590fdcc2b7b0ba846c06f313
kjs <rednada1486@gmail.com>
2023-02-28 15:24:39 +0900

    230228 coding study problem upload

---
+++ 230228/bfspra.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(graph, start):
+    pass
+
+
+bfs(graph, 'A')
+
+
