e87ec70702949a3371b855de4c660dafc120ecc2
kjs <rednada1486@gmail.com>
2023-02-28 15:25:45 +0900

    230228 coding study problem upload

---
+++ 230228/dfspra.py
@@ -0,0 +1,18 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def dfs(graph, start):
+    pass
+
+
+
+
+
+
+dfs(graph, 'A')
